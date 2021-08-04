ee250ae20745773b64e0328e6c7152bae8bf57cd
Chaldrak <chaldrakus@gmail.com>
2021-08-04 12:40:48 +0100

    ruby version changed

---
+++ app/bot/listener.rb
@@ -0,0 +1,20 @@
+# app/bot/listener.rb
+require 'facebook/messenger'
+
+include Facebook::Messenger
+
+class Listener
+  Facebook::Messenger::Subscriptions.subscribe(
+    access_token: ENV['ACCESS_TOKEN'],
+    subscribed_fields: %w[feed mention name]
+  )
+
+  Bot.on :message do |message|
+    Bot.deliver({
+      recipient: message.sender,
+      message: {
+        text: 'Uploading your message to skynet.'
+      }
+    }, access_token: ENV['ACCESS_TOKEN'])
+  end
+end
